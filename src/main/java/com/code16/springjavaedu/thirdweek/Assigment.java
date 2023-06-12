b21cead8abcf33e6a5aa029045bfa28bd729da5e
NesrinAsan <nesrin.asan@consultant.turkcell.com.tr>
2023-06-12 11:21:50 +0300

    #001 third week Assigment

---
+++ src/main/java/com/code16/springjavaedu/thirdweek/Assigment.java
@@ -0,0 +1,19 @@
+package com.code16.springjavaedu.thirdweek;
+
+public class Assigment {
+
+
+    /**
+     *  Bir havayolu şirketi bilet alma sistemi tasarlamamız gerekiyor.
+     *  Case 1: En az 2 farklı bilet hesaplama yöntemi olan bir uçuş ssistemi olmalı. Örnel: THY ve Pegasus gibi
+     *  Case2: Yolcu bilet almaya geldiğinde ona boş olan koltuklar gösterilmeli.
+     *  Bunun için koltyuk numaralandırılması önceden yapılabilir.
+     *  A1, A2, A3, B1,B2,B3 gibi koltuklar doldurulabilir.
+     *  Case 3: Her uçuş şirketinin yurtiçi ve yurtdışına özel davranışları olabili.
+     *  Örnek: Pegasus sadece yurtdışı uçuşlarında yemek veriyordur. Ama THY hem yurtiçi hem
+     *  yurtdışı uçuşlarda bu hizmeti sağlıyordur gibi.
+     *
+     *  Case4: Uçaklarda ekonomi ve business koltuğu olan uçaklar varsa hesaplama ona göre yapılmalı.
+     */
+
+}
