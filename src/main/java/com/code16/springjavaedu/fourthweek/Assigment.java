0fdb25cbfce839974f269de041146886ea5d0235
NesrinAsan <nesrin.asan@consultant.turkcell.com.tr>
2023-06-18 22:06:04 +0300

    #001 fourth week assigment

---
+++ src/main/java/com/code16/springjavaedu/fourthweek/Assigment.java
@@ -0,0 +1,13 @@
+package com.code16.springjavaedu.fourthweek;
+
+public class Assigment {
+
+
+	/**
+	 * 3. hafta verilen ödevin Database(h2 olabilr) API bağlanarak redesign edilmesi.
+	 * Bilet alma API'si, Koltuk seçme apisi, Rezervasyon iptal API gibi istediğiniz şekilde
+	 * çeşitlendirebilirsiniz.
+	 *
+	 * psvm altında yazdığınız kodu API'ye bağlamanız isteniyor.
+	 */
+}
